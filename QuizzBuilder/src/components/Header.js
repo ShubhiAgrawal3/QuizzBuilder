bab3623fa2ecf040d22f12c6f8314c00079d9cf3
gem-ShubhiAgrawal <Shubhi.Agrawal@Geminisolutions.com>
2022-05-26 17:05:53 +0530

    final commit

--- QuizzBuilder/src/components/Header.js
+++ QuizzBuilder/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div className="section">
+        <div className="section1">
         <div className="header">
             <h1>QuizzBuilder</h1>
         </div>
